a2dc4bd34dc1ca3d93f59b9bbb61baf31b783372
abdulawalemon1 <emondx4@gmail.com>
2022-05-10 01:43:17 +0600

    App updated

---
+++ app.py
@@ -0,0 +1,132 @@
+# Core Packages
+from audioop import reverse
+import imp
+from secrets import choice
+from tkinter import Menu
+from unittest import result
+import streamlit as st
+import streamlit.components.v1 as stc
+
+
+# Loading EDA
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
+# Load Dataset
+
+
+def load_data(data):
+    df = pd.read_csv(data)
+    return df
+# Fxn
+# Vectorize +Cosine Similarity Matrix
+
+
+def vectorize_text_to_cosine_mat(data):
+    coun_vect = CountVectorizer()
+    cv_mat = coun_vect.fit_transform(data)
+    # Get Cosine
+    cosine_sim_mat = cosine_similarity(cv_mat)
+    return cosine_sim_mat
+
+
+# Recommendation System
+@st.cache
+def get_recommendation(title, cosine_sim_mat, df, num_of_rec=5):
+    # indices of the course
+    courses_indices = pd.Series(
+        df.index, index=df['course_title']).drop_duplicates()
+    # Index of the course
+    idx = courses_indices[title]
+
+    # Look into the cosine matrix for that index
+    sim_scores = list(enumerate(cosine_sim_mat[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    selected_course_indices = [i[0] for i in sim_scores[1:]]
+    selected_course_scores = [i[0] for i in sim_scores[1:]]
+
+    # Get the dataframe & title
+    result_df = df.iloc[selected_course_indices]
+    result_df['similarity_score'] = selected_course_scores
+
+    final_recommended_coures = result_df[[
+        'course_title', 'similarity_score', 'url', 'price', 'num_subscribers']]
+    return final_recommended_coures.head(num_of_rec)
+
+
+# CSS Style
+RESULT_TEMP = """
+<div style="width:90%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;box-shadow:0 0 15px 5px solid #ccc;background-color:#a8f0c6;border-left:5px solid #6c6c6c;">
+
+<h4>{}</h4>
+<p style="color:blue;"><span style="color:black;">Score:</span>{}</p>
+<p style="color:blue;"><span style="color:black;"></span><a href="{}", target="_blank">Link</a></p>
+
+<p style="color:blue;"><span style="color:black;">Price:</span>{}</p>
+
+<p style="color:blue;"><span style="color:black;">Students:</span>{}</p>
+
+</div>
+"""
+
+# Search For Course
+
+
+@st.cache
+def search_item_if_not_found(term, df):
+    result_df = df[df['course_title'].str.contains(term)]
+    return result_df
+
+
+def main():
+    st.title("Course Recommendation App")
+    menu = ["Home", "Recommend", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    df = load_data("data/udemy_courses.csv")
+    if choice == "Home":
+        st.subheader("Home")
+        st.dataframe(df.head(10))
+    elif choice == "Recommend":
+        st.subheader("Recommend Courses")
+        cosine_sim_mat = vectorize_text_to_cosine_mat(df['course_title'])
+        search_term = st.text_input("Search")
+        num_of_rec = st.sidebar.number_input("Number", 4, 30, 7)
+        if st.button("Recommend"):
+            if search_term is not None:
+                try:
+                    results = get_recommendation(
+                        search_term, cosine_sim_mat, df, num_of_rec)
+                    with st.expander("Results as JSON"):
+                        results_json = results.to_dict('index')
+                        st.write(results_json)
+
+                    for row in results.iterrows():
+                        rec_title = row[1][0]
+                        rec_score = row[1][1]
+                        rec_url = row[1][2]
+                        rec_price = row[1][3]
+                        rec_num_sub = row[1][4]
+
+                    # st.write("Title", rec_title)
+                        stc.html(RESULT_TEMP.format(rec_title, rec_score,
+                                                    rec_url, rec_price, rec_num_sub), height=350)
+
+                except:
+                    results = "Not Found"
+                    st.warning(results)
+                    st.info("Suggested Options include")
+                    result_df = search_item_if_not_found(search_term, df)
+                    st.dataframe(result_df)
+
+                # st.write(results)
+
+                # How To Maximize Your Profits Options Trading
+    else:
+        st.subheader("About")
+        st.text("Built with Streamlit & Pandas")
+
+
+if __name__ == '__main__':
+    main()
